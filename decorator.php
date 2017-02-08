48e06848991c54999de8c973fd3e1d51aff7c60f
Jakub Borowski <jakub@borowski.priv.pl>
2017-02-08 01:16:04 +0100

    changes in list of portfolio filter

---
+++ decorator.php
@@ -0,0 +1,36 @@
+<?php
+interface Logger {
+public function log($message);
+}
+class FileLogger implements Logger {
+    public function log($message) {
+        echo "<p>log to file - message: {$message}</p>";
+    }
+}
+abstract class DecoratorLogger implements Logger {
+    protected $logger;
+    public function __construct(Logger $logger) {
+        $this->logger = $logger;
+    }
+    abstract public function log($message);
+}
+class EmailLogger extends DecoratorLogger {
+    public function log($message) {
+        $this->logger->log($message);
+        echo "<p>log to email - message: {$message}</p>";
+    }
+}
+class PDFLogger extends DecoratorLogger {
+    public function log($message) {
+        $this->logger->log($message);
+        echo "<p>log to PDF - message: {$message}</p>";
+    }
+}
+
+$log = New FileLogger();
+$log = New EmailLogger($log);
+$log = New PDFLogger($log);
+
+$log->log("Saving File");
+
+?>
